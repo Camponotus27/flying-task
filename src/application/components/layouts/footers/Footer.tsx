c7c0a3d1ac61fd4324e882ed8c1e8e03161813f9
Camponotus27 <31333058+Camponotus27@users.noreply.github.com>
2021-10-20 13:52:43 -0300

    Update Footer.tsx

--- src/application/components/layouts/footers/Footer.tsx
+++ src/application/components/layouts/footers/Footer.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.css';
 
 export default function Footer() {
-	return <footer className={styles.footer}>Fliying task</footer>;
+	return <footer className={styles.footer}>Flying task</footer>;
 }
